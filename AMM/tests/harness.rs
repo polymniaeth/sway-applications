2b4d785571c555fb92675a215578f4687d0faeec
SwayStar123 <swayambhanded@gmail.com>
2024-07-21 11:39:03 +0530

    rearrange AMM project

---
+++ AMM/tests/harness.rs
@@ -0,0 +1,5 @@
+mod amm_contract;
+mod exchange_contract;
+mod swap_exact_input;
+mod swap_exact_output;
+mod test_utils;
---
+++ AMM/tests/test_utils/paths.rs
@@ -0,0 +1,14 @@
+pub const AMM_CONTRACT_BINARY_PATH: &str = "./AMM-contract/out/debug/AMM-contract.bin";
+pub const AMM_CONTRACT_STORAGE_PATH: &str =
+    "./AMM-contract/out/debug/AMM-contract-storage_slots.json";
+pub const ATOMIC_ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str = "./atomic-add-liquidity/out/debug/atomic-add-liquidity.bin";
+pub const EXCHANGE_CONTRACT_BINARY_PATH: &str =
+    "./exchange-contract/out/debug/exchange-contract.bin";
+pub const EXCHANGE_CONTRACT_STORAGE_PATH: &str =
+    "./exchange-contract/out/debug/exchange-contract-storage_slots.json";
+pub const MALICIOUS_EXCHANGE_CONTRACT_BINARY_PATH: &str =
+    "./malicious-implementation/out/debug/malicious-implementation.bin";
+pub const MALICIOUS_EXCHANGE_CONTRACT_STORAGE_PATH: &str =
+    "./malicious-implementation/out/debug/malicious-implementation-storage_slots.json";
+pub const SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH: &str = "./swap-exact-input/out/debug/swap-exact-input.bin";
+pub const SWAP_EXACT_OUTPUT_SCRIPT_BINARY_PATH: &str = "./swap-exact-output/out/debug/swap-exact-output.bin";
